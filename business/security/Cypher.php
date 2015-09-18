fa408eff900e9c2181c3db9f87b035a32ab9fe75
jenspapenhagen <jens.papenhagen@web.de>
2015-09-18 16:29:50 +0200

    better mcrypt_create_iv

--- business/security/Cypher.php
+++ business/security/Cypher.php
@@ -5,7 +5,7 @@ class Cypher {
 
     function __construct($textkey) {
         $this->securekey = hash('sha256',$textkey,TRUE);
-        $this->iv = mcrypt_create_iv(32);
+        $this->iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC), MCRYPT_RAND);
     }
     
     function encrypt($input) {
